196dc7c82020d9604539ed8782441d534c7306de
edwin-chan-gtomato <edwin.y.chan@gtomato.com>
2021-04-19 17:47:11 +0800

    fix: errors

--- src/subDir/subDir2/subSubMain.ts
+++ src/subDir/subDir2/subSubMain.ts
@@ -1,12 +1,8 @@
-var foo = 1
-console.log(foo)
-var bar
-bar = 1
-function test(
-
-
-
-    ) {
-  console.log(baz)
+var foo = 1;
+console.log(foo);
+var bar;
+bar = 1;
+function Test() {
+    console.log(baz);
 }
-var baz = 123
+var baz = 123;
